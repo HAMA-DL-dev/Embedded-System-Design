b72d176a489ef852c78292fac52fe14b98bae160
OPhD <59410853+HAMA-DL-dev@users.noreply.github.com>
2022-06-21 16:57:38 +0900

    Delete OBDrhapsody directory

--- OBDrhapsody/Core/Inc/wifi_ubidots.h
+++
@@ -1,5 +0,0 @@
-
-extern void my_itoa(uint32_t val, uint8_t* str, uint8_t len);
-extern void FloatToString(uint8_t *str, float f, char size, char after_point);
-extern void WifiSetup(void);
-extern void WifiUbidots(void);
